66c737b9583be161eaac2b166f7cd9f96b725a89
wupeng1228 <wu12peng@>
2019-11-05 13:11:09 +0800

    add 00000

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,11 @@ public class HelloWorld {
 		System.out.println("22222");
 		System.out.println("33333");
 		System.out.println("44444");
-		System.out.println("33");
-		System.out.println("44");
         System.out.println("55555");
 		System.out.println("66666");
 		System.out.println("77777");
         System.out.println("88888");
+		System.out.println("99999");
+		System.out.println("00000");
     }
 }
