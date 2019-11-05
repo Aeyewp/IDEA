d7fc2108b4e8752483db40c0456193aaf243d0bc
wup <Aeyewp@163.com>
2019-11-05 13:24:22 +0800

    提交 123456

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,9 +6,5 @@ public class HelloWorld {
 		System.out.println("44444");
         System.out.println("55555");
 		System.out.println("66666");
-		System.out.println("77777");
-        System.out.println("88888");
-		System.out.println("11111");
-		System.out.println("22222");
     }
 }
