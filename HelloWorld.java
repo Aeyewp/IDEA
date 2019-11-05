336e65b6b220bd676056e01e63c1a3aef1df9056
wupeng1228 <wu12peng@>
2019-11-05 11:53:22 +0800

    update 22

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 		System.out.println("11111");
 		System.out.println("22222");
-		System.out.println("33333");
+		System.out.println("33");
 		System.out.println("44444");
         System.out.println("55555");
 		System.out.println("77777");
