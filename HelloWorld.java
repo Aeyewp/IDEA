0df572d2578d925163b5bda165fb2f3add5ae95b
wupeng1228 <wu12peng@>
2019-11-05 13:28:16 +0800

    提交356

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
 		System.out.println("11111");
 		System.out.println("22222");
 		System.out.println("33333");
+		System.out.println("33333");
+		System.out.println("55555");
+		System.out.println("66666");
     }
 }
